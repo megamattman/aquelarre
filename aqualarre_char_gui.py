e724dcd45fe06447cbbfa3753192c3712bfc7c98
Matt Hall <matthew_hall@sn.scee.net>
2017-05-17 15:21:22 +0100

    More refactoring widget drawing much cleaner now

--- aqualarre_char_gui.py
+++ aqualarre_char_gui.py
@@ -26,11 +26,73 @@ profession_options = sorted([option for option in professions.keys()])
 
 frame_order = ['vitals', 'characteristics', 'skills']
 
+def update():
+    #for label in characteristic_label_strings:
+    #    print "{} = {}".format(label, characteristic_map[label]['var'].get())
+    total_points = characteristic_map['total_points'].get('var')
+    total_points.set( "Total points: {}".format(total_characteristics()))
+    print total_points.get()
+
+def clear():
+    for key, val in characteristic_map.items():
+        if 'int' in val.get('type', ''):
+            val.get('var').set(0)
+        else :
+            val.get('var').set('')
+
+
+def event_handler (event, widget_name):
+    print widget_name
+
+
+#This map should contain data and pointers to the widgets
+characteristic_map = {
+    'character_name': {'frame': 'vitals'},
+    'kingdom'       : {'frame': 'vitals', 'options': kingdom_options},
+    'people'        : {'frame': 'vitals', 'options': people_options},
+    'profession'    : {'frame': 'vitals', 'options': profession_options},
+    'Strength'      : {'frame': 'characteristics', 'Name' : 'Strength',      'value' :  5, 'type' : 'int'},
+    'Agility'       : {'frame': 'characteristics', 'Name' : 'Agility',       'value' :  5, 'type' : 'int'},
+    'Dexterity'     : {'frame': 'characteristics', 'Name' : 'Dexterity',     'value' :  5, 'type' : 'int'},
+    'Resistance'    : {'frame': 'characteristics', 'Name' : 'Resistance',    'value' : 10, 'type' : 'int'},
+    'Perception'    : {'frame': 'characteristics', 'Name' : 'Perception',    'value' :  5, 'type' : 'int'},
+    'Communication' : {'frame': 'characteristics', 'Name' : 'Communication', 'value' :  5, 'type' : 'int'},
+    'Culture'       : {'frame': 'characteristics',                           'value' :  5, 'type' : 'int'},
+    'IRR'           : {'frame': 'derived',                           'value' :  50, 'type' : 'int'},
+    'RR'            : {'frame': 'derived',                           'value' :  50, 'type' : 'int'},
+    'luck'          : {'frame': 'derived',                           'value' :  15, 'type' : 'int'},
+    'Appearance'    : {'frame': 'derived',                           'value' :  15, 'type' : 'int'}
+}
+
 #Layout map
 frame_map = {
+    'controls'        :{
+        'self_config' : {'padding' : "8 0 0 0" },
+        'grid_config' : {'row' : 0, 'column': 0, 'sticky' : 'N, W'},#'columnspan' : 2},
+        'content_config' :{
+            'clear' : {
+                'button' : {
+                    'grid_config' : {'row' : 0, 'column' : 0, 'sticky' : 'N, W'},
+                    'self_config' : {'width' : 15, 'text' : 'clear' , 'command' : clear }
+                }
+            },
+            'update' : {
+                'button' : {
+                    'grid_config' : {'row' : 0, 'column' : 1, 'sticky' : 'N, W'},
+                    'self_config' : {'width' : 15, 'text' : 'update' , 'command' : update }
+                }
+            },
+            'total_points' : {
+                'label' : {
+                    'grid_config' : {'row' : 0, 'column' : 2, 'sticky' : 'N, W'},
+                    'self_config' : {'width' : 15, 'text' : 'Total Points', 'textvariable' : None}
+                }
+            }
+        }
+    },
     'vitals'          : {
-        'self_config' : {'padding' : "8 8 8 8" },
-        'grid_config' : {'row' : 0, 'column': 0, 'sticky' : 'N, W' },#'columnspan' : 2},
+        'self_config' : {'padding' : "8 0 0 0" },
+        'grid_config' : {'row' : 1, 'column': 0, 'sticky' : 'N, W','columnspan' : 3},
         'content_config' : {
             'character_name' : {
                 'label' : {
@@ -47,27 +109,27 @@ frame_map = {
                     'grid_config' : {'row' : 0, 'column' : 2, 'sticky' : 'N, W'},
                     'self_config' : {'width' : 15, 'text' : 'Kingdom'}
                 },
-                'options' : {
+                'option_menu' : {
                     'grid_config' : {'row' : 0, 'column' : 3, 'sticky' : 'N, W'},
                     'self_config' : {'width' : 15}
-                    }
+                }
             },
             'people' : {
                 'label' : {
                     'grid_config' : {'row' : 1, 'column' : 0, 'sticky' : 'N, W'},
                     'self_config' : {'width' : 15. , 'text' : 'People'}
                 },
-                'options' : {
+                'option_menu' : {
                     'grid_config' : {'row' : 1, 'column' : 1, 'sticky' : 'N, W'},
                     'self_config' : {'width' : 15}
-                    }
+                }
             },
             'profession' : {
                 'label' : {
                     'grid_config' : {'row' : 1, 'column' : 2, 'sticky' : 'N, W'},
                     'self_config' : {'width' : 15, 'text' : 'Profession'}
                 },
-                'options' : {
+                'option_menu' : {
                     'grid_config' : {'row' : 1, 'column' : 3, 'sticky' : 'N, W'},
                     'self_config' : {'width' : 25}
                 }
@@ -76,36 +138,28 @@ frame_map = {
     },
     'skills'          : {
         'grid_config' : {
-            'row' : 1, 'column': 1, 'sticky' : 'E'
+            'row' : 2, 'column': 2, 'sticky' : 'E'
         }
     },
     'characteristics' : {
         'grid_config' : {
-            'row' : 1, 'column': 0, 'sticky' : 'W'
+            'row' : 2, 'column': 0, 'sticky' : 'W'
+        }
+    },
+    'derived' : {
+        'grid_config' : {
+            'row' : 2, 'column': 1, 'sticky' : 'W'
         }
     }
     #'vitals' : {'row' : 0, 'column': 0, 'width' : 0, 'height': 0},
     #'vitals' : {'row' : 0, 'column': 0, 'width' : 0, 'height': 0},
 }
 
-#Allow accessing the elements of the form through a single map
-characteristic_map = {
-    'character_name': {'frame': 'vitals'},
-    'kingdom'       : {'frame': 'vitals', 'options': kingdom_options},
-    'people'        : {'frame': 'vitals', 'options': people_options},
-    'profession'    : {'frame': 'vitals', 'options': profession_options},
-    'Strength'      : {'frame': 'characteristics', 'Name' : 'Strength',      'value' :  5},
-    'Agility'       : {'frame': 'characteristics', 'Name' : 'Agility',       'value' :  5},
-    'Dexterity'     : {'frame': 'characteristics', 'Name' : 'Dexterity',     'value' :  5},
-    'Resistance'    : {'frame': 'characteristics', 'Name' : 'Resistance',    'value' : 10},
-    'Perception'    : {'frame': 'characteristics', 'Name' : 'Perception',    'value' :  5},
-    'Communication' : {'frame': 'characteristics', 'Name' : 'Communication', 'value' :  5},
-    'Culture'       : {'frame': 'characteristics', 'Name' : 'Culture',       'value' :  5},
-}
 characteristic_map.update(skills)
 
-#ordered list used for displaying
-characteristic_label_strings = [
+def total_characteristics():
+    total = 0
+    strings_to_total = [
     'Strength',
     'Agility',
     'Dexterity',
@@ -113,20 +167,8 @@ characteristic_label_strings = [
     'Perception',
     'Communication',
     'Culture'
-]
-
-def update():
-    #for label in characteristic_label_strings:
-    #    print "{} = {}".format(label, characteristic_map[label]['var'].get())
-    total_points=StringVar()
-    total_points.set( "Total points: {}".format(total_characteristics()))
-    Label(mainframe, text=total_points.get()).grid(column=6,row=1,sticky=(E))
-    characteristic_map['kingdom']['label'].configure(background='blue')
-    #characteristic_map['Strength']['label'].background = 'blue'
-
-def total_characteristics():
-    total = 0
-    for label in characteristic_label_strings:
+    ]
+    for label in strings_to_total:
         value = characteristic_map[label]['var'].get()
         try:
             total += value
@@ -134,22 +176,38 @@ def total_characteristics():
             print ("NO")
     return total
 
-def pop (event):
-    print ("aaaaf")
-    print ("pressed", repr(event.char))
+def pop (*args):
+    for key, val in characteristic_map.items():
+        try:
+            value = val['var'].get()
+            if value:
+                print "{} : {}".format(key, value)
+        except:
+            print "failed to get value of {}".format(key)
 
-def create_frame_content(frame_name, name_list):
+def pip (event):
+    print "pip"
+    pprint (event)
+    print event.widget
+
+def get_tk_var (var_type) :
+    if 'int' in var_type:
+        return IntVar()
+    else:
+        return StringVar()
+
+def create_frame_content(frame_name, name_list, rows, cols):
     frame_description = frame_map.get(frame_name)
     for idx, name in enumerate(name_list):
         new_content = {
             name :{
                 'label' : {
-                    'grid_config' : {'row' :  0 + (idx % 10), 'column' : 0 + ((idx / 10)*2), 'sticky' : "W"},
+                    'grid_config' : {'row' :  0 + (idx % rows), 'column' : 0 + ((idx / cols)*2), 'sticky' : "W"},
                     'self_config' : {'width' : 15, 'text' : name}
                     },
                  'entry' : {
-                     'grid_config' : {'row' :  0 + ((idx % 10)), 'column' :  1 + ((idx / 10)*2), 'sticky' : "W"},
-                     'self_config' : {'width' : 3}
+                     'grid_config' : {'row' :  0 + ((idx % rows)), 'column' :  1 + ((idx / cols)*2), 'sticky' : "W"},
+                     'self_config' : {'width' : 3, 'textvariable' : None}
                     }
                 }
             }
@@ -157,33 +215,45 @@ def create_frame_content(frame_name, name_list):
             frame_description['content_config'] = {}
         frame_description['content_config'].update(new_content)
 
+def get_empty_widget (content_type, frame, content_data):
+    if 'option_menu' in content_type :
+        content_options = content_data.get('options', {})
+        return OptionMenu(frame, content_data['var'],  content_options[0], *content_options)
+    if 'button' in content_type :
+        return Button(frame)
+    if 'label' in content_type:
+        return Label(frame)
+    if 'entry' in content_type:
+        return Entry(frame)
 
 
 def populate_frame(frame_name, configs):
-    print frame_name
+    print "populating: {}".format(frame_name)
     frame = frame_map.get(frame_name).get('frame')
-    pprint (configs.keys())
     for content_name , content_config in configs.items():
-        print "content name:{}".format(content_name)
+        if content_name not in characteristic_map.keys():
+            characteristic_map[content_name] = {}
         content_data = characteristic_map.get(content_name)
-        content_data['var'] = StringVar()
-        #pprint(content_config)
+        content_data['var'] = get_tk_var(content_data.get('type', ''))
+        content_data['var'].set(content_data.get('value', ''))
+
         for content_type, content_configs in content_config.items():
-            print "content_type {}".format(content_type)
-            if 'options' in content_type:
-                content_options = content_data.get('options', {})
-                options_menu = OptionMenu(frame, content_data['var'],  content_options[0], *content_options)
-                options_menu.configure(**content_configs.get('self_config', {}))
-                options_menu.grid(**content_configs.get('grid_config', {}))
-                characteristic_map['options_menu'] = options_menu
-            if 'label' in content_type:
-                label = Label(frame, **content_configs.get('self_config', {}))
-                label.grid(**content_configs.get('grid_config', {}))
-                content_data['label'] = label
-            if 'entry' in content_type:
-                entry = Entry (frame, textvariable=content_data['var'], **content_configs.get('self_config', {}))
-                entry.grid(**content_configs.get('grid_config', {}))
-                content_data['entry'] = entry
+            new_content = get_empty_widget(content_type, frame, content_data)
+            def handler (event, name=content_name) :
+                return event_handler(event, name)
+
+            if 'textvariable' in content_configs.get('self_config').keys():
+                content_configs.get('self_config')['textvariable'] = content_data['var']
+
+            if 'option_menu' in content_type:
+                content_data['var'].trace("w", pop)
+            elif 'entry' in content_type:
+                new_content.bind('<FocusOut>', handler)
+
+            if new_content is not None :
+                new_content.grid(**content_configs.get('grid_config', {}))
+                new_content.configure(**content_configs.get('self_config', {}))
+                content_data[content_type] = new_content
 
 def create_frames (master, name, configs):
     new_frame = Frame(master, **configs.get('self_config', {}))
@@ -201,17 +271,21 @@ if __name__ == "__main__":
 
     for name, configs in frame_map.items():
         create_frames(root, name, configs)
+    characteristic_label_strings = [key for key, val in characteristic_map.items() if 'characteristics' in val.get('frame','')]
+    derived_label_strings = [key for key, val in characteristic_map.items() if 'derived' in val.get('frame','')]
 
-    create_frame_content('characteristics', characteristic_label_strings)
-    create_frame_content('skills', skills)
+    create_frame_content('characteristics', characteristic_label_strings, 7, 7)
+    create_frame_content('derived', derived_label_strings, 7, 7)
+    create_frame_content('skills', skills, 7, 7)
 
     #pprint (frame_map)
     #exit()
 
-    #for frame_name, configs in frame_map.items():
-    for frame_name in frame_order:
+    for frame_name, configs in frame_map.items():
+    #for frame_name in frame_order:
         print "populate"
-        populate_frame(frame_name, frame_map.get(frame_name).get('content_config', {}))
+        populate_frame(frame_name, configs.get('content_config', {}))
+        #populate_frame(frame_name, frame_map.get(frame_name).get('content_config', {}))
 
         #if 'characteristics' in frame_name:
         #    draw_characteristics(frame, name , configs)
